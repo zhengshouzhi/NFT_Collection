9fd00e2c9da0d5e96803c98e073e384b22ec5476
zhengshouzhi <54531163@qq.com>
2022-11-05 15:24:07 +0800

    eliminate white arrow

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xe0EC4526500d7693226739BFE964D597D7ce8ED2";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
